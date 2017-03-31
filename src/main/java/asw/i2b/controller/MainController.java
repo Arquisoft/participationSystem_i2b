f043dc705e226578ba39dc0deaa58a127b56b76f
Jorge López <nokutu@openmailbox.org>
2017-03-31 09:15:23 +0200

    Fixed missing attribute in home view

--- src/main/java/asw/i2b/controller/MainController.java
+++ src/main/java/asw/i2b/controller/MainController.java
@@ -2,6 +2,7 @@ package asw.i2b.controller;
 
 
 import asw.i2b.dao.ProposalsRepository;
+import asw.i2b.dao.dto.Category;
 import asw.i2b.dao.dto.Proposal;
 import asw.i2b.model.Message;
 import asw.i2b.model.ProposalCreation;
@@ -17,7 +18,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @Controller
 public class MainController {
@@ -51,6 +54,8 @@ public class MainController {
     public String send(Model model) {
         model.addAttribute("proposals", proposalService.getProposalsByPopularity());
         model.addAttribute("createProposal", new ProposalCreation());
+        // TODO add services
+        model.addAttribute("categories", new ArrayList<Category>());
         return "user/home";
     }
 
