3b0d91ca15c20620f119454d2fee02e07452d762
miguelgrc <miguelparo12@hotmail.com>
2017-04-01 21:14:27 +0200

    proposals.json modified for adding comments (maybe not working)

--- src/test/java/asw/i2b/CucumberSteps.java
+++ src/test/java/asw/i2b/CucumberSteps.java
@@ -115,6 +115,7 @@ public class CucumberSteps {
                         .append("body", proposal.getString("body"))
                         .append("votes", proposal.getInt("votes"))
                         .append("votedUsernames", proposal.getJSONArray("votedUsernames").toList())
+                        .append("comments", proposal.getJSONArray("comments"))
                 );
             });
 
